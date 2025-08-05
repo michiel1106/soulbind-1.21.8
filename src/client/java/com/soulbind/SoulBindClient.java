758cdfdcd8c241e918954674d92e05786a5ad296
michiel1106 <michielimmink@gmail.com>
2025-08-05 20:54:37 +0200

    Initial commit

---
+++ src/client/java/com/soulbind/SoulBindClient.java
@@ -0,0 +1,10 @@
+package com.soulbind;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SoulBindClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
